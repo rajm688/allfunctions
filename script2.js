81f1ad5767fe72831c13e0fb2f9b697123728644
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-11-24 15:10:19 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,65 @@
+// let b = 5
+// let c = 6
+// let a = b+c
+// console.log(a)
+
+// function sum(b,c){
+//     let a = b+c
+//     console.log(a)
+// }
+// sum(5,6)
+
+// function sum(b,c){
+//      return   b+c
+// }
+// console.log(sum(5,6))
+
+
+// let sum =function (b,c){
+//     return   b+c
+// }
+// console.log(sum(5,6))
+
+// (function (a,b){
+//     console.log(a+b); 
+// })(5,6)
+
+// let sum = () => console.log("hello")
+// sum()
+// let  sum = (a,b) => console.log(a+b)
+
+// sum(6,5);
+
+
+// function displayval(display){
+//     console.log(display)
+// }
+// function sum(a,b){
+//     return a+b;
+// }
+// let value = sum(5,6)
+// displayval(value)
+
+// function displayval(display){
+//     console.log(display)
+// }
+// function sum(a,b){
+//     let value = a+b;
+//     displayval(value)
+// }
+// sum(5,6)
+
+// function displayval(display){
+//     // setTimeout(() => {
+//     //     console.log(display) 
+//     // }, 3000);
+//     console.log(display) 
+   
+// }
+// function sum(a,b,callback){
+//    setTimeout(() => {
+//     let value = a+b;
+//     callback(value)
+//    }, 3000);
+// }
+// sum(5,6,displayval)
